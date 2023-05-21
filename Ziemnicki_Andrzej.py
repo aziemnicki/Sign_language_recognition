82efdc772f12331b40243f96bded3402a8c68917
aziemnicki <a-ziemnicki@wp.pl>
2023-05-21 02:58:35 +0200

    Data read, arguments

--- Ziemnicki_Andrzej.py
+++ Ziemnicki_Andrzej.py
@@ -3,8 +3,14 @@ import argparse
 import json
 from pathlib import Path
 import pandas as pd
-
-#raw_data = pd.read_csv('WZUM dataset.csv')
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC
 
 
 def main():
@@ -16,8 +22,13 @@ def main():
     dataset_path = Path(args.dataset_path)
     results_file = Path(args.results_file)
     raw_data = pd.read_csv(dataset_path)
-
-
+    print(raw_data.info())
+    print(raw_data.describe())
+    #TODO 'letter;
+    data = raw_data.copy()
+    Y = data['letter']
+    X = data.drop('letter', axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42, stratify=Y)
 
 
 
